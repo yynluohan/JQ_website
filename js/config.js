c00a44a2a31e9c4441807c9a97abe0aa0c330526
ruohan <1534007973@qq.com>
2019-03-31 18:50:57 +0800

    websit init

---
+++ js/config.js
@@ -0,0 +1,9 @@
+; (function (window) {
+  // window.MC = { HOST: 'http://120.79.77.207:8080' };
+  window.MC = { HOST: 'http://120.79.77.207:8080/portal-api' };
+  // window.MC = { HOST: 'https://www.muaskin.com' };
+
+  //兼容性问题测试工具
+  // https://www.cnblogs.com/zamhown/p/6686278.html
+  // https://blog.csdn.net/yaya_1q2w/article/details/79449015
+})(window);
