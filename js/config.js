7460ada7d257414e017706e0c801cc8df54519c6
ruohan <1534007973@qq.com>
2019-04-07 22:52:43 +0800

    fix conflict and push

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 ; (function (window) {
   // window.MC = { HOST: 'http://120.79.77.207:8080' };
-  // window.MC = { HOST: 'http://120.79.77.207:8080/portal-api' };
-  window.MC = { HOST: 'https://www.muaskin.com' };
+
+  window.MC = { HOST: 'http://120.79.77.207:8080/portal-api' };
+  // window.MC = { HOST: 'https://www.muaskin.com' };
 
   //兼容性问题测试工具
   // https://www.cnblogs.com/zamhown/p/6686278.html
